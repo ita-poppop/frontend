8e5b07c9d7e5fcf042089e8d16f044e3831e8a4d
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2025-07-07 17:36:39 +0900

    ISSUE-27[feat] 댓글 게시, 삭제 api 추가

--- ita/app/src/main/java/com/ita/poppop/data/remote/api/CommentApi.kt
+++ ita/app/src/main/java/com/ita/poppop/data/remote/api/CommentApi.kt
@@ -1,9 +1,16 @@
 package com.ita.poppop.data.remote.api
 
+import com.ita.poppop.data.remote.dto.comments.DeleteCommentResponse
 import com.ita.poppop.data.remote.dto.comments.GetCommentListResponse
 import com.ita.poppop.data.remote.dto.comments.GetCommentResponse
+import com.ita.poppop.data.remote.dto.comments.PostCommentRequest
+import com.ita.poppop.data.remote.dto.comments.PostCommentResponse
 import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.POST
 import retrofit2.http.Path
 import retrofit2.http.Query
 
@@ -21,4 +28,17 @@ interface CommentApi {
         @Path("reviewId") reviewId: Int,
         @Path("commentId") commentId: Int
     ): Response<GetCommentResponse>
+
+    @POST("/api/v1/popups/{popupId}/reviews/{reviewId}/comments")
+    suspend fun postComment(
+        @Header("Authorization") accessToken: String,
+        @Path("reviewId") reviewId: Int,
+        @Body request: PostCommentRequest
+    ): Response<PostCommentResponse>
+
+    @DELETE("/api/v1/popups/{popupId}/reviews/{reviewId}/comments/{commentId}/delete")
+    suspend fun deleteComment(
+        @Header("Authorization") accessToken: String,
+        @Path("commentId") commentId: Int
+    ): Response<DeleteCommentResponse>
 }
